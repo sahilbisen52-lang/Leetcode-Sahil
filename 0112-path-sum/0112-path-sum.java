e4397265720ec9c1821e3b26c9b2101979041263
CoreLogicX <sahilbisen52@gmail.com>
2026-07-07 15:24:47 +0530

    Time: 0 ms (100%), Space: 45.1 MB (14.67%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root==null){
+            return false;
+        }
+        if(root.left==null && root.right==null){
+            return targetSum==root.val;
+        }
+        int newtarget=targetSum-root.val;
+
+        boolean leftpath=hasPathSum(root.left,newtarget);
+        boolean rightpath=hasPathSum(root.right,newtarget);
+
+        return leftpath||rightpath;
+        
+    }
+}
